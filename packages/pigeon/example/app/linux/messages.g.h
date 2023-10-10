1d77290c52d6eb2d09629fb1003840f6a35bd707
Robert Ancell <robert.ancell@canonical.com>
2023-10-10 15:24:12 +1300

    Update generated Linux code

--- packages/pigeon/example/app/linux/messages.g.h
+++ packages/pigeon/example/app/linux/messages.g.h
@@ -6,163 +6,67 @@
 
 #ifndef PIGEON_MESSAGES_G_H_
 #define PIGEON_MESSAGES_G_H_
-#include <flutter/basic_message_channel.h>
-#include <flutter/binary_messenger.h>
-#include <flutter/encodable_value.h>
-#include <flutter/standard_message_codec.h>
 
-#include <map>
-#include <optional>
-#include <string>
+#include <flutter_linux/flutter_linux.h>
 
-// Generated class from Pigeon.
+G_BEGIN_DECLS
 
-class FlutterError {
- public:
-  explicit FlutterError(const std::string& code)
-    : code_(code) {}
-  explicit FlutterError(const std::string& code, const std::string& message)
-    : code_(code), message_(message) {}
-  explicit FlutterError(const std::string& code, const std::string& message, const flutter::EncodableValue& details)
-    : code_(code), message_(message), details_(details) {}
+G_DECLARE_FINAL_TYPE(MyMessageData, my_message_data, MY, MESSAGE_DATA, GObject)
 
-  const std::string& code() const { return code_; }
-  const std::string& message() const { return message_; }
-  const flutter::EncodableValue& details() const { return details_; }
+typedef enum { MY_CODE_ONE = 0, MY_CODE_TWO = 1 } MyCode;
 
- private:
-  std::string code_;
-  std::string message_;
-  flutter::EncodableValue details_;
+MyMessageData* my_message_data_new(MyCode code, FlValue* data);
+
+MyMessageData* my_message_data_new_full(const gchar* name,
+                                        const gchar* description, MyCode code,
+                                        FlValue* data);
+
+const gchar* my_message_data_get_name(MyMessageData* object);
+
+const gchar* my_message_data_get_description(MyMessageData* object);
+
+MyCode my_message_data_get_code(MyMessageData* object);
+
+FlValue* my_message_data_get_data(MyMessageData* object);
+
+G_DECLARE_DERIVABLE_TYPE(MyExampleHostApi, my_example_host_api, MY,
+                         EXAMPLE_HOST_API, GObject)
+
+struct _MyExampleHostApiClass {
+  GObjectClass parent_class;
+
+  void (*get_host_language)(MyExampleHostApi* object,
+                            FlMethodCall* method_call);
+  void (*add)(MyExampleHostApi* object, FlMethodCall* method_call, int64_t a,
+              int64_t b);
+  void (*send_message)(MyExampleHostApi* object, FlMethodCall* method_call,
+                       MyMessageData* message);
 };
 
-template<class T> class ErrorOr {
- public:
-  ErrorOr(const T& rhs) : v_(rhs) {}
-  ErrorOr(const T&& rhs) : v_(std::move(rhs)) {}
-  ErrorOr(const FlutterError& rhs) : v_(rhs) {}
-  ErrorOr(const FlutterError&& rhs) : v_(std::move(rhs)) {}
+void my_example_host_api_respond_get_host_language(MyExampleHostApi* object,
+                                                   FlMethodCall* method_call,
+                                                   const gchar* result);
 
-  bool has_error() const { return std::holds_alternative<FlutterError>(v_); }
-  const T& value() const { return std::get<T>(v_); };
-  const FlutterError& error() const { return std::get<FlutterError>(v_); };
+void my_example_host_api_respond_add(MyExampleHostApi* object,
+                                     FlMethodCall* method_call, int64_t result);
 
- private:
-  friend class ExampleHostApi;
-  friend class MessageFlutterApi;
-  ErrorOr() = default;
-  T TakeValue() && { return std::get<T>(std::move(v_)); }
+void my_example_host_api_respond_send_message(MyExampleHostApi* object,
+                                              FlMethodCall* method_call,
+                                              gboolean result);
 
-  std::variant<T, FlutterError> v_;
-};
+G_DECLARE_FINAL_TYPE(MyMessageFlutterApi, my_message_flutter_api, MY,
+                     MESSAGE_FLUTTER_API, GObject)
 
+MyMessageFlutterApi* my_message_flutter_api_new();
 
-enum class Code {
-  one = 0,
-  two = 1
-};
-// Generated class from Pigeon that represents data sent in messages.
-class MessageData {
- public:
-  // Constructs an object setting all non-nullable fields.
-  explicit MessageData(
-    const Code& code,
-    const flutter::EncodableMap& data);
+void my_message_flutter_api_flutter_method_async(MyMessageFlutterApi* object,
+                                                 const gchar* a_string,
+                                                 GCancellable* cancellable,
+                                                 GAsyncReadyCallback callback,
+                                                 gpointer user_data);
 
-  // Constructs an object setting all fields.
-  explicit MessageData(
-    const std::string* name,
-    const std::string* description,
-    const Code& code,
-    const flutter::EncodableMap& data);
-
-  const std::string* name() const;
-  void set_name(const std::string_view* value_arg);
-  void set_name(std::string_view value_arg);
-
-  const std::string* description() const;
-  void set_description(const std::string_view* value_arg);
-  void set_description(std::string_view value_arg);
-
-  const Code& code() const;
-  void set_code(const Code& value_arg);
-
-  const flutter::EncodableMap& data() const;
-  void set_data(const flutter::EncodableMap& value_arg);
-
-
- private:
-  static MessageData FromEncodableList(const flutter::EncodableList& list);
-  flutter::EncodableList ToEncodableList() const;
-  friend class ExampleHostApi;
-  friend class ExampleHostApiCodecSerializer;
-  friend class MessageFlutterApi;
-  friend class MessageFlutterApiCodecSerializer;
-  std::optional<std::string> name_;
-  std::optional<std::string> description_;
-  Code code_;
-  flutter::EncodableMap data_;
-
-};
-
-class ExampleHostApiCodecSerializer : public flutter::StandardCodecSerializer {
- public:
-  ExampleHostApiCodecSerializer();
-  inline static ExampleHostApiCodecSerializer& GetInstance() {
-    static ExampleHostApiCodecSerializer sInstance;
-    return sInstance;
-  }
-
-  void WriteValue(
-    const flutter::EncodableValue& value,
-    flutter::ByteStreamWriter* stream) const override;
-
- protected:
-  flutter::EncodableValue ReadValueOfType(
-    uint8_t type,
-    flutter::ByteStreamReader* stream) const override;
-
-};
-
-// Generated interface from Pigeon that represents a handler of messages from Flutter.
-class ExampleHostApi {
- public:
-  ExampleHostApi(const ExampleHostApi&) = delete;
-  ExampleHostApi& operator=(const ExampleHostApi&) = delete;
-  virtual ~ExampleHostApi() {}
-  virtual ErrorOr<std::string> GetHostLanguage() = 0;
-  virtual ErrorOr<int64_t> Add(
-    int64_t a,
-    int64_t b) = 0;
-  virtual void SendMessage(
-    const MessageData& message,
-    std::function<void(ErrorOr<bool> reply)> result) = 0;
-
-  // The codec used by ExampleHostApi.
-  static const flutter::StandardMessageCodec& GetCodec();
-  // Sets up an instance of `ExampleHostApi` to handle messages through the `binary_messenger`.
-  static void SetUp(
-    flutter::BinaryMessenger* binary_messenger,
-    ExampleHostApi* api);
-  static flutter::EncodableValue WrapError(std::string_view error_message);
-  static flutter::EncodableValue WrapError(const FlutterError& error);
-
- protected:
-  ExampleHostApi() = default;
-
-};
-// Generated class from Pigeon that represents Flutter messages that can be called from Linux.
-class MessageFlutterApi {
- public:
-  MessageFlutterApi(flutter::BinaryMessenger* binary_messenger);
-  static const flutter::StandardMessageCodec& GetCodec();
-  void FlutterMethod(
-    const std::string* a_string,
-    std::function<void(const std::string&)>&& on_success,
-    std::function<void(const FlutterError&)>&& on_error);
-
- private:
-  flutter::BinaryMessenger* binary_messenger_;
-};
+gboolean my_message_flutter_api_flutter_method_finish(
+    MyMessageFlutterApi* object, GAsyncResult* result, gchar** value,
+    GError** error);
 
 #endif  // PIGEON_MESSAGES_G_H_
